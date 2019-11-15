a80ecc91e5e862292b6841cda9fc765f00d0a75c
lorenzOliveto <lorenzo.oliveto@gmail.com>
2019-11-15 12:24:22 +0100

    .gitignore is now working

--- build/MBAdmin.build/Release-iphoneos/MBAdmin.build/DerivedSources/MBAdmin_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char MBAdminVersionString[];
- extern const double MBAdminVersionNumber;
-
- const unsigned char MBAdminVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:MBAdmin  PROJECT:MBAdmin-1" "\n";
- const double MBAdminVersionNumber __attribute__ ((used)) = (double)1.;
